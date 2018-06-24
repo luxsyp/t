d19c5d07e3b27de79bdb3265ae93ae6156014b8e
d.locque <d.locque@meetic-corp.com>
2018-06-24 17:32:41 +0200

    add SplashScreen

--- app/src/main/java/john/snow/transferwise/ui/SplashScreenActivity.kt
+++ app/src/main/java/john/snow/transferwise/ui/SplashScreenActivity.kt
@@ -3,14 +3,25 @@ package john.snow.transferwise.ui
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import john.snow.rickandmorty.ui.list.CharacterListActivity
 import john.snow.transferwise.R
+import kotlinx.android.synthetic.main.activity_splashscreen.*
+
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val rotateAnimation = RotateAnimation(0f, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
+        rotateAnimation.duration = DELAY_SPLASH_SCREEN
+        splashIconImageView.startAnimation(rotateAnimation)
+
 
         Handler().postDelayed({
             CharacterListActivity.start(this)
