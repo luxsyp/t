6e2cd00f98e5d4482c415a80cbea586e69938fed
d.locque <d.locque@meetic-corp.com>
2018-06-24 20:09:45 +0200

    add annotation for tests

--- rickandmorty/src/main/java/john/snow/rickandmorty/detail/repository/CharacterDetailsRepository.kt
+++ rickandmorty/src/main/java/john/snow/rickandmorty/detail/repository/CharacterDetailsRepository.kt
@@ -4,6 +4,8 @@ import john.snow.rickandmorty.model.RMCharacter
 
 
 interface CharacterDetailsRepository {
+
+    @Throws(CharacterDetailsException::class)
     fun getCharacter(characterId: Int): RMCharacter
 
     class CharacterDetailsException(message: String? = null, cause: Throwable? = null) : Throwable(message, cause)
